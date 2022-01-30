6949173ab966a92c5fb7123567e7a7f2bfd6f9ec
parlador <jean.olivier.bergeron@gmail.com>
2022-01-30 14:38:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from threading import Event
 import sys
 import struct
 import pygame
+import pygame_gui
 import os
 from logging import getLogger
 from dcsbios import ProtocolParser, StringBuffer, IntegerBuffer
@@ -14,8 +15,30 @@ from dcsbios import ProtocolParser, StringBuffer, IntegerBuffer
 pygame.init()
 pygame.mouse.set_visible(False)
  
-screen = pygame.display.set_mode((800, 480))
+window_surface = pygame.display.set_mode((800, 480))
+background = pygame.Surface((800, 480))
+background.fill(pygame.Color('#000000'))
 
-screen.fill((0,255,0))
+manager = pygame_gui.UIManager((800, 480))
 
-pygame.display.update()
+hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                            text='Say Hello',
+                                            manager=manager)
+
+clock = pygame.time.Clock()
+is_running = True
+
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+    pygame.display.update()
